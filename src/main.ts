2dea86e2e7499e42475f1163ac90a96825070e96
Thomas Perche <thomas.perche@symphonyretailai.com>
2023-11-22 17:51:56 +0100

    Adding SWC compiler, fastify adaptater

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ http2: true })
+  )
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
