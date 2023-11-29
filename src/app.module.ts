1c08d48df1c12b51a51deaf8ef9a99a179d18b61
Thomas Perche <thomas.perche@symphonyretailai.com>
2023-11-29 17:40:59 +0100

    Inject the filter globally to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
+import { APP_FILTER } from '@nestjs/core';
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { ArticlesModule } from './articles/articles.module';
+import { ExceptionsLoggerFilter } from './common/utils/exceptionLogger.filter';
+
 
 @Module({
   imports: [ArticlesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide: APP_FILTER,
+    useClass: ExceptionsLoggerFilter,
+  },
+    AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
