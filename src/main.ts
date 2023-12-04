069d96f2f9f906749b3ecb61628f35cb7bd62e36
Thomas Perche <thomas.perche@symphonyretailai.com>
2023-12-04 17:27:08 +0100

    Update package.json

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './logging.interceptor';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,10 +14,11 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true })
   )
   app.useGlobalInterceptors(new LoggingInterceptor());
+  const configService = app.get(ConfigService);
 
   // By default, Fastify listens only on the localhost 127.0.0.1 interface.
   // If you want to accept connections on other hosts, you should specify '0.0.0.0' in the listen() call
-  await app.listen(3000, '127.0.0.1');
+  await app.listen(configService.get('PORT'), '127.0.0.1');
 }
 
-bootstrap();
+bootstrap();      
